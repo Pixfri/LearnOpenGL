6a5806b37452f20f8f560ab6d5af603e3dd671fd
Pixfri <jean.letessier@protonmail.com>
2023-03-10 00:01:15 +0100

    Added light casters

--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -128,6 +128,19 @@ int main() {
                 -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
         };
 
+        glm::vec3 cubePositions[] = {
+                glm::vec3( 0.0f,  0.0f,  0.0f),
+                glm::vec3( 2.0f,  5.0f, -15.0f),
+                glm::vec3(-1.5f, -2.2f, -2.5f),
+                glm::vec3(-3.8f, -2.0f, -12.3f),
+                glm::vec3( 2.4f, -0.4f, -3.5f),
+                glm::vec3(-1.7f,  3.0f, -7.5f),
+                glm::vec3( 1.3f, -2.0f, -2.5f),
+                glm::vec3( 1.5f,  2.0f, -2.5f),
+                glm::vec3( 1.5f,  0.2f, -1.5f),
+                glm::vec3(-1.3f,  1.0f, -1.5f)
+        };
+
         unsigned int VBO, cubeVAO;
         glGenVertexArrays(1, &cubeVAO);
         glGenBuffers(1, &VBO);
@@ -183,45 +196,73 @@ int main() {
                 glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
                 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-                // activate shader
+                // be sure to activate shader when setting uniforms/drawing objects
                 lightingShader.use();
+                lightingShader.setVec3("light.position", camera.Position);
+                lightingShader.setVec3("light.direction", camera.Front);
+                lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+                lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
+
                 lightingShader.setVec3("viewPos", camera.Position);
 
+                // light properties
                 lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-                lightingShader.setVec3("light.position", lightPos);
+                lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
                 lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 
+                lightingShader.setFloat("light.constant", 1.0f);
+                lightingShader.setFloat("light.linear", 0.09f);
+                lightingShader.setFloat("light.quadratic", 0.032f);
+
+                // material properties
                 lightingShader.setFloat("material.shininess", 32.0f);
 
-                // pass projection matrix to shader (note that in this case it could change every frame)
+                // view/projection transformations
                 glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
                 glm::mat4 view = camera.GetViewMatrix();
                 lightingShader.setMat4("projection", projection);
                 lightingShader.setMat4("view", view);
 
+                // world transformation
                 glm::mat4 model = glm::mat4(1.0f);
                 lightingShader.setMat4("model", model);
 
-                // bind texture to corresponding texture unit
+                // bind diffuse map
                 glActiveTexture(GL_TEXTURE0);
                 glBindTexture(GL_TEXTURE_2D, diffuseMap);
-
+                // bind specular map
                 glActiveTexture(GL_TEXTURE1);
                 glBindTexture(GL_TEXTURE_2D, specularMap);
 
-                glBindVertexArray(cubeVAO);
-                glDrawArrays(GL_TRIANGLES, 0, 36);
+                // render the cube
+                // glBindVertexArray(cubeVAO);
+                // glDrawArrays(GL_TRIANGLES, 0, 36);*/
 
-                lightCubeShader.use();
+                // render containers
+                glBindVertexArray(cubeVAO);
+                for (unsigned int i = 0; i < 10; i++) {
+                        // calculate the model matrix for each object and pass it to shader before drawing
+                        glm::mat4 model = glm::mat4(1.0f);
+                        model = glm::translate(model, cubePositions[i]);
+                        float angle = 20.0f * i;
+                        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+                        lightingShader.setMat4("model", model);
+
+                        glDrawArrays(GL_TRIANGLES, 0, 36);
+                }
+
+
+                /*lightCubeShader.use();
                 lightCubeShader.setMat4("projection", projection);
                 lightCubeShader.setMat4("view", view);
                 model = glm::mat4(1.0f);
                 model = glm::translate(model, lightPos);
-                model = glm::scale(model, glm::vec3(0.2f));
+                model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
                 lightCubeShader.setMat4("model", model);
 
                 glBindVertexArray(lightCubeVAO);
-                glDrawArrays(GL_TRIANGLES, 0, 36);
+                glDrawArrays(GL_TRIANGLES, 0, 36);*/
+
 
                 // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
                 // -------------------------------------------------------------------------------
